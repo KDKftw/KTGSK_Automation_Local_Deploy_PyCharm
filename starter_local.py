3e3e2dcbb7be0583b91326837a1a40c3b9e74162
KDKftw <ooo.kadoun@gmail.com>
2022-04-20 13:13:43 +0200

    updates for ktgsk

--- starter_local.py
+++ starter_local.py
@@ -23,16 +23,16 @@ def suite():
     suite.addTest(TestDetailHotelu_C("test_detail_terminy_filtr_airport"))
     suite.addTest(TestDetskeKluby_D("test_kluby_D"))
     suite.addTest(TestFM_D("test_FM_D"))
-    suite.addTest(Test_Fulltext_C("test_fulltext_naseptavac"))
-    suite.addTest(Test_Fulltext_C("test_fulltext_results_status_check"))
+    #suite.addTest(Test_Fulltext_C("test_fulltext_naseptavac"))
+    #suite.addTest(Test_Fulltext_C("test_fulltext_results_status_check"))
     suite.addTest(Test_Groupsearch_D("test_groupsearch_D"))
     suite.addTest(TestHP_D("test_homePage_D"))
     suite.addTest(TestLM_D("test_lM_isDisplayed"))
     suite.addTest(TestPobocky_D('test_pobocky_D'))
-    suite.addTest(TestPoznavacky_D('test_poznavacky_okruzni_D'))
-    suite.addTest(TestPoznavacky_D('test_poznavacky_vikendy_D'))
-    suite.addTest(TestPoznavacky_D('test_poznavacky_rodiny_D'))
-    suite.addTest(TestPoznavacky_D('test_poznavacky_zazitky_D'))
+    #suite.addTest(TestPoznavacky_D('test_poznavacky_okruzni_D'))
+    #suite.addTest(TestPoznavacky_D('test_poznavacky_vikendy_D'))
+    #suite.addTest(TestPoznavacky_D('test_poznavacky_rodiny_D'))
+    #suite.addTest(TestPoznavacky_D('test_poznavacky_zazitky_D'))
     suite.addTest(TestSDO_D('test_SDO_D'))
     suite.addTest(Test_SRL_C('test_SRL_sort_cheapest'))
     suite.addTest(Test_SRL_C('test_SRL_sort_expensive'))
@@ -58,7 +58,7 @@ if __name__ == '__main__':
 
     #runner = HtmlTestRunner(title='My unit test', open_in_browser=True)
     #runner = HtmlTestRunner.HTMLTestRunner(output='example_dir')        ## this is ??
-    runner = HtmlTestRunner.HTMLTestRunner(log=True, verbosity=2, output='EXIM Web Suite test', title='EXIM Web Suite test', report_name='EXIM Web Suite test',
-                            open_in_browser=True, description="EXIM Web Suite testt")
+    runner = HtmlTestRunner.HTMLTestRunner(log=True, verbosity=2, output='KARTAGOSK Web Suite test', title='KARTAGOSK Web Suite test', report_name='KARTAGOSK Web Suite test',
+                            open_in_browser=True, description="KARTAGOSK Web Suite testt")
     ####  pip install HTMLTestRunner-rv
     runner.run(suite())
