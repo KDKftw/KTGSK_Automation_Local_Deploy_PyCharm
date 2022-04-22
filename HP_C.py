ab1670bb611c253312c11ecfcd1dbdec8c2ff761
KDKftw <ooo.kadoun@gmail.com>
2022-04-22 20:05:40 +0200

    no message

--- HP_C.py
+++ HP_C.py
@@ -1,13 +1,13 @@
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.wait import WebDriverWait
-from to_import import acceptConsent, URL, setUp, tearDown
+from to_import import acceptConsent, URL, setUp, tearDown, generalDriverWaitImplicit
 import unittest
 from selenium.webdriver.support import expected_conditions as EC
 from groupsearch_D import groupSearch_D
 import time
 
 HPvyhledatZajezdyButtonXpath = "//*[contains(text(), 'Vyhledat zájezdy')]"
-HPkamPojedeteButtonXpath = "//*[contains(text(), 'Kam na dovolenou?')]"
+HPkamPojedeteButtonXpath = "//*[contains(text(), 'Kam se chystáte?')]"
 HPzlutakReckoDestinaceXpath = "//*[@class='f_input-content'] //*[contains(text(), 'Řecko')]"
 #HPzlutakReckoDestinaceXpath = "/html/body[@id='homepage']/header[@class='f_pageHeader js_header f_set--filterOpened']/div[@class='f_pageHeader-content']/div[@class='f_pageHeader-item f_pageHeader-item--holder']/div/div[@class='f_filterMainSearch']/div/div[2]/span/div[@class='f_filterHolder f_set--active']/div[@class='f_filterHolder-content']/div[@class='f_filter f_filter--destination']/div[@class='f_customScroll js_destinationsContent']/div[1]/div[@class='f_column']/div[@class='f_column-item'][1]/div[@class='f_list']/div[@class='f_list-item'][1]/div[@class='f_input-wrapper']/label[@class='f_input f_input--checkbox']/span[@class='f_input-content']"
 HPzlutakPokracovatButtonXpath = "//*[contains(text(), 'Pokračovat')]"
@@ -18,6 +18,8 @@ HPzlutakLetniPrazdninyXpath = "//*[contains(text(), 'Letní prázdniny 2022')]"
 HPzlutakPridatPokojXpath = "//*[contains(text(), 'přidat pokoj')]"
 HPzlutakObsazenost2plus1Xpath = "//*[contains(text(), 'Rodina 2+1')]"
 HPzlutakPotvrditAvyhledatXpath = "//*[@class='f_button f_button--common'] //*[contains(text(), 'Potvrdit a vyhledat')]"
+HPnejlepsiZajezdySwitchButtonXpath = "//*[@class='f_switch-button']"
+HPnejlepsiZajezdyVypisXpath = "//*[@class='f_tourTable-tour']"
 class Test_HP_C(unittest.TestCase):
     def setUp(self):
         setUp(self)
@@ -26,21 +28,24 @@ class Test_HP_C(unittest.TestCase):
         tearDown(self)
 
     def test_HP_zlutak_to_groupsearch(self):
+        self.driver.maximize_window()
         self.driver.get(URL)
         wait = WebDriverWait(self.driver, 300)
-        self.driver.maximize_window()
+
         acceptConsent(self.driver)
         wait.until(EC.visibility_of(self.driver.find_element_by_xpath(HPvyhledatZajezdyButtonXpath))).click()
-        time.sleep(2.5)     ##time sleep not the best not pog but it works =)
+        time.sleep(2.5)  ##time sleep not the best not pog but it works =)
         groupSearch_D(self, self.driver)
 
     def test_HP_zlutak_to_SRL(self):
+        self.driver.maximize_window()
         self.driver.get(URL)
         wait = WebDriverWait(self.driver, 300)
-        self.driver.maximize_window()
-        acceptConsent(self.driver)
-        wait.until(EC.visibility_of(self.driver.find_element_by_xpath(HPkamPojedeteButtonXpath))).click()
 
+        acceptConsent(self.driver)
+        time.sleep(1)
+        wait.until(EC.visibility_of(self.driver.find_element_by_xpath(HPkamPojedeteButtonXpath))).click()
+        time.sleep(0.3)
         wait.until(EC.visibility_of(self.driver.find_element_by_xpath(HPzlutakReckoDestinaceXpath))).click()
 
         wait.until(EC.visibility_of(self.driver.find_element_by_xpath(HPzlutakPokracovatButtonXpath))).click()
@@ -51,10 +56,69 @@ class Test_HP_C(unittest.TestCase):
         time.sleep(0.5)
         wait.until(EC.visibility_of(self.driver.find_element_by_xpath(HPzlutakPokracovatButtonXpathStep2))).click()
 
-
         wait.until(EC.visibility_of(self.driver.find_element_by_xpath(HPzlutakObsazenost2plus1Xpath))).click()
 
         time.sleep(1)
         wait.until(EC.visibility_of(self.driver.find_element_by_xpath(HPzlutakPotvrditAvyhledatXpath))).click()
 
         time.sleep(20)
+
+    def test_HP_nejlepsi_nabidky_vypis_btn_switch(self):
+        self.driver.get(URL)
+        wait = WebDriverWait(self.driver, 300)
+        self.driver.maximize_window()
+        time.sleep(
+            0.3)  ##this is to workaround accept consent since in maximizes and then selenium gets confused with clickin on the element
+        acceptConsent(self.driver)
+        generalDriverWaitImplicit(self.driver)
+        wait.until(EC.visibility_of(self.driver.find_element_by_xpath(HPnejlepsiZajezdyVypisXpath)))
+        nejlepsiNabidkyElement = self.driver.find_elements_by_xpath(HPnejlepsiZajezdyVypisXpath)
+        positionOfCurrentElement = 0
+        nejlepsiNabidkyTextList = []
+        for _ in nejlepsiNabidkyElement:
+            nejlepsiNabidkyTextDefault = nejlepsiNabidkyElement[positionOfCurrentElement].text
+            nejlepsiNabidkyTextList.append(nejlepsiNabidkyTextDefault)
+            # print (nejlepsiNabidkyTextList)
+            positionOfCurrentElement = positionOfCurrentElement + 1
+
+        wait.until(EC.visibility_of(self.driver.find_element_by_xpath(HPnejlepsiZajezdySwitchButtonXpath)))
+        HPnejlepsiZajezdySwitchButtonElement = self.driver.find_element_by_xpath(HPnejlepsiZajezdySwitchButtonXpath)
+        self.driver.execute_script("arguments[0].click();", HPnejlepsiZajezdySwitchButtonElement)
+        time.sleep(6)
+        self.driver.implicitly_wait(10)
+        nejlepsiNabidkyElement = self.driver.find_elements_by_xpath(HPnejlepsiZajezdyVypisXpath)
+        positionOfCurrentElement2 = 0
+        nejlepsiNabidkyTextList2 = []
+        for _ in nejlepsiNabidkyElement:
+            nejlepsiNabidkyTextDefault = nejlepsiNabidkyElement[positionOfCurrentElement2].text
+            nejlepsiNabidkyTextList2.append(nejlepsiNabidkyTextDefault)
+            # print(nejlepsiNabidkyTextList)
+            positionOfCurrentElement2 = positionOfCurrentElement2 + 1
+
+        print(nejlepsiNabidkyTextList)
+        print(nejlepsiNabidkyTextList2)
+        assert nejlepsiNabidkyTextList != nejlepsiNabidkyTextList2
+
+    def test_HP_slider_click_detail_hotelu(self):
+        self.driver.get(URL)
+        wait = WebDriverWait(self.driver, 300)
+        self.driver.maximize_window()
+        time.sleep(
+            0.3)  ##this is to workaround accept consent since in maximizes and then selenium gets confused with clickin on the element
+        acceptConsent(self.driver)
+        # wait.until(EC.visibility_of(self.driver.find_element_by_xpath(HPnextArrowXpath))).click()
+        # time.sleep(10)
+        self.driver.implicitly_wait(100)
+        topNabidkaBigHotelCardXpath = "//*[@class='page-widget js-ajaxPlaceholder--widget fshr-widget f_tileGrid-item f_tileGrid-item--double']"
+        topNabidkaBigHotelCardElement = self.driver.find_element_by_xpath(topNabidkaBigHotelCardXpath)
+
+        self.driver.execute_script("arguments[0].scrollIntoView();", topNabidkaBigHotelCardElement)
+        # self.driver.execute_script("arguments[0].scrollIntoView();", HPkartaHoteluSliderElement)
+        # action.move_to_element(HPkartaHoteluSliderElement).click().perform()
+        self.driver.implicitly_wait(100)
+        time.sleep(6)
+        topNabidkaBigHotelCardElement.click()
+        time.sleep(2)
+        curURL = self.driver.current_url
+
+        assert curURL != URL
